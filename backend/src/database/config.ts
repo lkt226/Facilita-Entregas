e68cdba7cfe91fa17ab3af20f45f5eeeb7d72cea
Victor Hugo <83098581+lkt226@users.noreply.github.com>
2024-03-11 13:01:03 -0300

    removendo teste

--- backend/src/database/config.ts
+++ backend/src/database/config.ts
@@ -7,13 +7,4 @@ const client = new Client ({
   port: parseInt(process.env.POSTGRES_PORT || '5432')
 })
 
-// TESTE PARA VER O RESULTADO QUE DA NO RENDER ( NÃO ACHANDO O BANCO DE DADOS )
-console.log({
-  user: process.env.POSTGRES_USER || 'facilita-juridico',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  password: process.env.POSTGRES_PASSWORD || 'admin12345',
-  port: parseInt(process.env.POSTGRES_PORT || '5432'),
-  database: process.env.POSTGRES_DATABASE || 'mydb'
-})
-
 export default client
